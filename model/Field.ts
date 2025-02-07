42a43048f64335ac9ed2dc28e1adcefa2a2364e5
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-02-07 23:25:04 +0530

    Field model created

---
+++ model/Field.ts
@@ -0,0 +1,9 @@
+export default class Field {
+    fieldId!: string;
+    fieldName!: string;
+    fieldExtent!: string;
+    fieldLocation!: string;
+    fieldImage1!: string;
+    fieldImage2!: string;
+}
+
