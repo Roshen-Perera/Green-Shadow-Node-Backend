a5aa86636cefc1308ade5520f9bf1357683f7a0f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-02-09 20:03:45 +0530

    Crops model done

---
+++ model/Crop.ts
@@ -0,0 +1,9 @@
+export default class Crops {
+    cropId!: string;
+    cropName!: string;
+    cropScientificName!: string;
+    cropCategory!: string;
+    cropSeason!: string;
+    cropImage1!: string;
+    cropFieldId!: string;
+}
