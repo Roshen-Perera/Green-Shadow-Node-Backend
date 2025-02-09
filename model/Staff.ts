36ddd63bc1f0c97484ca74051541a10cc004dbd7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-02-10 00:35:05 +0530

    Staff model created

---
+++ model/Staff.ts
@@ -0,0 +1,18 @@
+export interface Staff {
+    staffId: string;
+    firstName: string;
+    lastName: string;
+    designation: string;
+    gender: string;
+    joinedDate: string;
+    dob: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    addressLine4: string;
+    addressLine5: string;
+    contactNo: string;
+    email: string;
+    role: string;
+    fieldCode: string;
+}
