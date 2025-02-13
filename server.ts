b9d96c7c0ec64d5f9740e126e1a669c9d3d559d2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-02-13 20:42:22 +0530

    log route added to server.ts

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import fieldRoutes from "./routes/field-routes";
 import cropRoutes from "./routes/crop-routes";
 import staffRoutes from "./routes/staff-routes";
+import logRoutes from "./routes/log-routes";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use('/',  (req, res,next) => {
 app.use('/field',fieldRoutes);
 app.use('/crop',cropRoutes);
 app.use('/staff',staffRoutes );
+app.use('/log',logRoutes );
 
 app.listen(3000, (err=>{
     console.log("Server running on port 3000");
