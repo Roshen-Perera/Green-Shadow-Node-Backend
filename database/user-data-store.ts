56b5a75e8d2e6e284e4705231d67fbdf933bc270
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-02-26 11:43:46 +0530

    User data store added

---
+++ database/user-data-store.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import {User} from "../model/User";
+import bcrypt from "bcrypt";
+
+const prisma = new  PrismaClient();
+
+export async function createUser(user: User){
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const addUser = await prisma.user.create({
+        data: {
+            username: user.username,
+            password: hashedPassword
+        }
+    });
+    console.log('User Added :',addUser);
+}
+
+export async function verifyUser(verifyUser: User) {
+    const user : User | null = await prisma.user.findUnique({
+        where: { username: verifyUser.username }, // Use the username as the unique identifier to find the relevant user
+    });
+    if (!user) { // If the user doesn't exist
+        return false;
+    }
+    return await bcrypt.compare(verifyUser.password, user.password); // Compare the provided password with the stored hashed password
+}
--- model/Log.ts
+++
@@ -1,9 +0,0 @@
-export default class Log {
-    logId!: string;
-    logDate!: string;
-    details!: string;
-    observedImage!: string;
-    logFieldId!: string;
-    logCropId!: string;
-    logStaffId!: string;
-}
